97509154551bab57a0ddbf7c431f325e5c30d939
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-18 23:03:10 +0530

    dns

--- server.tf
+++ server.tf
@@ -14,6 +14,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z001151113ESNLT809BTY"
+  name    = "frontend-dev.nandu18.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
@@ -22,6 +30,13 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z001151113ESNLT809BTY"
+  name    = "mongodb-dev.nandu18.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
 
 resource "aws_instance" "catalogue" {
@@ -32,6 +47,13 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 }
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z001151113ESNLT809BTY"
+  name    = "catalogue-dev.nandu18.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
 
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
@@ -40,6 +62,13 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+resource "aws_route53_record" "redis" {
+  zone_id = "Z001151113ESNLT809BTY"
+  name    = "redis-dev.nandu18.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 
 
@@ -51,6 +80,13 @@ resource "aws_instance" "user" {
     Name = "user"
   }
 }
+resource "aws_route53_record" "user" {
+  zone_id = "Z001151113ESNLT809BTY"
+  name    = "user-dev.nandu18.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
 
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
@@ -59,6 +95,13 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+resource "aws_route53_record" "cart" {
+  zone_id = "Z001151113ESNLT809BTY"
+  name    = "cart-dev.nandu18.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
 }
 
 
@@ -70,6 +113,13 @@ resource "aws_instance" "mysql" {
     Name = "mysql"
   }
 }
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z001151113ESNLT809BTY"
+  name    = "mysql-dev.nandu18.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
 
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
@@ -78,6 +128,13 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z001151113ESNLT809BTY"
+  name    = "shipping-dev.nandu18.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
 }
 
 resource "aws_instance" "rabbitmq" {
@@ -88,6 +145,13 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z001151113ESNLT809BTY"
+  name    = "rabbitmq-dev.nandu18.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
 
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
@@ -96,4 +160,11 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+resource "aws_route53_record" "payment"
+  zone_id = "Z001151113ESNLT809BTY"
+  name    = "payment-dev.nandu18.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
