1a5763934f2dfb008bb86050d275805f476ee25f
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-18 21:58:29 +0530

    ip

--- server.tf
+++ server.tf
@@ -7,6 +7,10 @@ resource "aws_instance" "frontend" {
   }
 }
 
+output "frontend" {
+  value = aws_instance.frontend.public_ip
+}
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
