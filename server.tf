95c12de80c66e6b5d67b4dd1b8c04ea3d7963c76
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-18 22:27:35 +0530

    correction

--- server.tf
+++ server.tf
@@ -1,12 +1,14 @@
-data "aws_ami"  "centos"  {
+data "aws_ami"  "centos" {
   owners          = ["973714476881"]
   most_recent     = true
-  name_regex      = "centos-8-DevOps-practice"
+  name_regex      = "centos-8-DevOps-Practice"
 }
 
+
 output "ami" {
-  value = data.aws_ami.centos.image_id
+  value =data.aws_ami.centos.image_id
 }
+
 
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
