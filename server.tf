6ad7ce151ed2bb700e4362e24853915f89ba2729
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-18 22:14:54 +0530

    mistake

--- server.tf
+++ server.tf
@@ -1,5 +1,5 @@
-data "aws_ami" "centos" {
-  owners          =["973714476881"]
+data "aws_ami"  "centos"  {
+  owners          = ["973714476881"]
   most_recent     = true
   name_regex      = "centos-8-DevOps-practice"
 }
