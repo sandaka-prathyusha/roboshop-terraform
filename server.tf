a23f86ba8dfcdbe0fd89634afae18655375b7248
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-08-12 11:27:15 +0530

    push

--- server.tf
+++ server.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
-    Name each.value["name"]
+    Name = each.value["name"]
   }
 
 
