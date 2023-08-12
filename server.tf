40ba3c253786e0bea509ffcaab1885eab6bcd3bd
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-08-12 10:42:02 +0530

    push

--- server.tf
+++ server.tf
@@ -1,8 +1,8 @@
 
 resource "aws_instance" "instance" {
-  for_each      = var.components
-  ami           = data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
@@ -10,29 +10,32 @@ resource "aws_instance" "instance" {
   }
 
 
-#
-#connection can be within provisioner or outside
+  #
+  #connection can be within provisioner or outside
+  resource "null_resource" "provisioner" {
+    depends_on = [aws_instance.instance,aws_route53_record]
+#wait for these particular to be created then only run provisioner.
+    for_each               = var.components #how many times i want to run variables#
 
-  provisioner "remote-exec" {
+    provisioner "remote-exec" {
 
-    connection {
-     type     = "ssh"
-     user     = "centos"
-     password = "DevOps321"
-     host     = self.private-ip
-   }
-  inline = [
-    #i have to declare list of commands here
-    "rm -rf roboshop-shell",
-    "git clone https://github.com/nandu18/roboshop-shell",
-    "cd roboshop-shell",
-    "sudo bash ${each.value["name"]}.sh"
+      connection {
+        type     = "ssh"
+        user     = "centos"
+        password = "DevOps321"
+        host     = [aws_instance.instance[each.value["name"]].private_ip]
+      }
+      inline = [
+        #i have to declare list of commands here
+        "rm -rf roboshop-shell",
+        "git clone https://github.com/nandu18/roboshop-shell",
+        "cd roboshop-shell",
+        "sudo bash ${each.value["name"]}.sh"
 
-  ]
- }
+      ]
+    }
+  }
 }
-
-
 
 resource "aws_route53_record" "records" {
   for_each = var.components
